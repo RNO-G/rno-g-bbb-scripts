cc806da20ef185560bb41a14bc1cd1549563d83d
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2022-08-22 12:55:58 -0500

    raw wind mon script

---
+++ i2c/raw-wind.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+while true ; 
+do
+
+  outname=/tmp/windmon.`date`.log
+
+  for n in {1..60} 
+  do
+    echo START >> $outname
+
+    for i in {1..50} ; 
+    do
+      echo $i  >> $outname
+      date +%s.%N >> $outname
+      i2ctransfer -y 2 r200@0x4e >> $outname
+      date +%s.%N >> $outname
+      sleep 0.01
+    done
+    echo END >> $outname
+    sleep 60
+  done
+  xz $outname && mv $outname.xz /data/windmon
+done
+
