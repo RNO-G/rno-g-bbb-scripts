8924244dd5cf6e8a504f328410f2bc3688a522f0
Cosmin Deaconu <cozzyd@gmail.com>
2024-05-13 08:15:54 -0500

    revify

--- gpio/select-ext-i2c.sh
+++ gpio/select-ext-i2c.sh
@@ -2,5 +2,11 @@
 
 if [ ! -d /sys/class/gpio/gpio67 ]; then echo 67 > /sys/class/gpio/export; fi
 
+rev=`cat /REV` 
+if [[ $rev == "F" ]] 
+then 
+echo high > /sys/class/gpio/gpio67/direction
+else  
 echo low > /sys/class/gpio/gpio67/direction
+fi
 
--- gpio/unselect-ext-i2c.sh
+++ gpio/unselect-ext-i2c.sh
@@ -2,5 +2,11 @@
 
 if [ ! -d /sys/class/gpio/gpio67 ]; then echo 67 > /sys/class/gpio/export; fi
 
+rev=`cat /REV` 
+if [[ $rev == "F" ]] 
+then 
+echo low > /sys/class/gpio/gpio67/direction
+else
 echo in > /sys/class/gpio/gpio67/direction
+fi
 
